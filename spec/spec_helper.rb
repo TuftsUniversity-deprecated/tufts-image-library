7751f93baa03279e754189178bee58ee9abb81f5
Mike Korcynski <mkorcy@gmail.com>
2016-02-08 15:58:26 -0500

    adds ldap sample and ladle to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'ladle'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -18,9 +19,14 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+
   config.include Devise::TestHelpers, type: :controller
 
   config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    @ldap_server = Ladle::Server.new(:quiet => false, :ldif => File.expand_path('../fixtures/tufts_ldap.ldif', __FILE__)).start
+  end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
