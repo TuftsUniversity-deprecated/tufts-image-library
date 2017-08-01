3cc88e1a5044673bea2a8ce8523a941dbe5dbe88
Mike Korcynski <mkorcy@gmail.com>
2017-08-01 13:26:02 -0400

    remove temporal from shove view

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -69,7 +69,6 @@ class CatalogController < ApplicationController
     config.add_show_field solr_name('date_formatted', :stored_searchable, type: :string), label: 'Date'
     config.add_show_field solr_name('description', :stored_searchable, type: :string), label: 'Description'
     config.add_show_field solr_name('spatial', :stored_searchable), label: 'Location depicted'
-    config.add_show_field solr_name('temporal', :stored_searchable), label: 'Time period'
     config.add_show_field solr_name('genre', :stored_searchable), label: 'Genre'
     config.add_show_field solr_name('subject', :stored_searchable), label: 'Subject'
 
